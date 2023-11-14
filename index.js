66b3a6d934e876ee6602fd39925bc4befb6be318
spython <emmanuelegbo01@gmail.com>
2023-11-14 15:29:34 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+
+app.use(cors());
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL, { 
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {console.error(err);});
+
+
+
+app.listen("6000", () =>{
+    console.log('Backend server is running');
+});
